177efa8fc5e8983e85f37619b6e3b6320d9e7f47
Gnomesenpai <toshua_4_eve@hotmail.co.uk>
2023-06-20 13:47:51 +0100

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -1,5 +1,5 @@
-cd /media/user/nsrl/ && \
-wget http://172.25.15.117/RDS_2023.06.1_modern_minimal.zip -O  nsrl.zip && \
+cd /nsrl && \
+mv RDS*.zip nsrl.zip
 unzip nsrl.zip
 rm nsrl.zip
 cd *minimal
@@ -8,7 +8,7 @@ mv -v "$nsrldb" ../backup.db
 cd ..
 #create TXT from DB file
 ls -lah
-sqlite3 ./backup.db <<'END_SQL'
+sqlite3 ./backup.db << 'END_SQL'
 DROP TABLE IF EXISTS EXPORT;
 CREATE TABLE EXPORT AS SELECT sha1, md5, crc32, file_name, file_size, package_id FROM FILE;
 UPDATE EXPORT SET file_name = REPLACE(file_name, '"', '');
@@ -18,12 +18,14 @@ UPDATE EXPORT SET file_name = REPLACE(file_name, '"', '');
 SELECT '"' || sha1 || '"', '"' || md5 || '"', '"' || crc32 || '"', '"' || file_name || '"', file_size, 
 package_id, '"' || 0 || '"', '"' || '"' FROM EXPORT ORDER BY sha1;
 .q
-sed -i 's/"""/"/g' output.txt
+sed -i "s/"""/"/g" output.txt
 #Create a header file with a single line containing the following, called NSRLFile-header.txt:
-echo "SHA-1","MD5","CRC32","FileName","FileSize","ProductCode","OpSystemCode","SpecialCode" > NSRLFile-header.txt
+echo '\"SHA-1\",\"MD5\",\"CRC32\",\"FileName\",\"FileSize\",\"ProductCode\",\"OpSystemCode\",\"SpecialCode\"' > NSRLFile-header.txt
+ls -lah
 cat output.txt >> NSRLFile-header.txt
+ls -lah
+sed -i 's/"""/"/g' NSRLFile-header.txt
 sed -e "s/\r//g" NSRLFile-header.txt > NSRLFile-$(date +"%d-%m-%Y").txt
 #reclaim space
-rm output.txt NSRLFile-header.txt
-#reclaim DB space/name
-DROP TABLE IF EXISTS EXPORT;
+rm output.txt
+rm NSRLFile-header.txt
