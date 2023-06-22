671f29b33a60c8818560c21236d9f088d00e6d92
Gnomesenpai <toshua_4_eve@hotmail.co.uk>
2023-06-22 15:37:18 +0100

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -1,13 +1,14 @@
 cd /nsrl && \
+#rename RDS zip to a standard name
 mv RDS*.zip nsrl.zip
 unzip nsrl.zip
-#rm nsrl.zip
+rm nsrl.zip
 cd *minimal
+#find DB file attach to a variable to rename
 nsrldb="$(ls *minimal.db | sort -V | tail -n1)"
-cp -v "$nsrldb" ../backup.db
+mv -v "$nsrldb" ../backup.db
 cd ..
 #create TXT from DB file
-ls -lah
 echo "Creating output.txt"
 ../sql.sh
 cd /nsrl
@@ -16,15 +17,16 @@ sed -i 's/"""/"/g' output.txt
 #Create a header file with a single line containing the following, called NSRLFile-header.txt:
 echo "create NSRLFile-header.txt"
 echo "\"SHA-1\",\"MD5\",\"CRC32\",\"FileName\",\"FileSize\",\"ProductCode\",\"OpSystemCode\",\"SpecialCode\"" > NSRLFile-header.txt
-ls -lah
 echo "cat output.txt into NSRLFile-header.txt"
 cat output.txt >> NSRLFile-header.txt
-ls -lah
 echo "sed NSRLFile-header.txt"
-#sed -i 's/"""/"/g' NSRLFile-header.txt
 echo "final file creation"
 sed -e "s/\r//g" NSRLFile-header.txt > NSRLFile-$(date +"%d-%m-%Y").txt
 #reclaim space
 rm output.txt
 rm NSRLFile-header.txt
-sha1sum NSRLFile-$(date +"%d-%m-%Y").txt
+rm backup.db
+rm -R RDS*
+echo "Hashing NSRLFile"
+sha1sum NSRLFile-$(date +"%d-%m-%Y").txt > NSRLFile-$(date +"%d-%m-%Y").hash
+cat NSRLFile-$(date +"%d-%m-%Y").hash
